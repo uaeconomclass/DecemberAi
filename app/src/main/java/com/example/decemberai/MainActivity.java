92ea51a313d02c09f02c6d39daec8298d6d6db13
AttilaMoroz <valentyn1983@yandex.ru>
2023-12-22 11:44:03 +0200

    Сменили версию апи на 24

--- app/src/main/java/com/example/decemberai/MainActivity.java
+++ app/src/main/java/com/example/decemberai/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.decemberai;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menu_lessons:
+                    // Обработка нажатия на Уроки
+                    return true;
+                case R.id.menu_practice:
+                    // Обработка нажатия на Практика
+                    return true;
+                case R.id.menu_progress:
+                    // Обработка нажатия на Прогресс
+                    return true;
+                case R.id.menu_profile:
+                    // Обработка нажатия на Профиль
+                    return true;
+                default:
+                    return false;
+            }
+        });
+
+
     }
 }
