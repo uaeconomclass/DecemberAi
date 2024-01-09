c21f7e60b3777bed502c740ee3331d377c1147e5
wowyc <149575541+wowyc@users.noreply.github.com>
2024-01-09 21:05:16 +0300

    Revert "тест"
    This reverts commit 7b5393e7438bf4023daa350507a1b8920e525810.

--- app/src/main/java/com/example/decemberai/SecondActivity.java
+++ app/src/main/java/com/example/decemberai/SecondActivity.java
@@ -1,27 +1,29 @@
 package com.example.decemberai;
 
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second); // Устанавливаем макет для SecondActivity
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.menu_lessons:
                     // Обработка нажатия на Уроки
-                    startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    startActivity(new Intent(SecondActivity.this, MainActivity.class));
                     return true;
                 case R.id.menu_practice:
                     // Обработка нажатия на Практика
-                    startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                    startActivity(new Intent(SecondActivity.this, SecondActivity.class));
                     return true;
                 case R.id.menu_progress:
                     // Обработка нажатия на Прогресс
@@ -32,7 +34,12 @@ public class MainActivity extends AppCompatActivity {
                 default:
                     return false;
             }
+
+
         });
+
+
+
 
 
     }
