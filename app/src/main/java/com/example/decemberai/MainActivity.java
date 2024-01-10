cfd4eb5d028ee8e736a8916eb1d089150aa33199
wowyc <149575541+wowyc@users.noreply.github.com>
2024-01-10 11:53:23 +0300

    botom_nav_and_back_staks_function_ready
    Остается визуал подправить

--- app/src/main/java/com/example/decemberai/MainActivity.java
+++ app/src/main/java/com/example/decemberai/MainActivity.java
@@ -1,85 +1,116 @@
 package com.example.decemberai;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.example.decemberai.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
+    BottomNavigationView bottomNavigationView;
+    Deque<Integer> integerDeque = new ArrayDeque<>(4);// Устанавливаем размер стека для хранения страниц
+    boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        // binding.bottomNavigationView.setSelectedItemId(R.id.button_practice); Это на всякий случай установка активного эллемента нижнего меню
-        replaceFragment(new Lessons_wFragment(), false); // При загрузке страницы сразу загружаемся с Лекции
 
-        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        integerDeque.push(R.id.btn_home);
+        loadFragment(new HomeFragment());
+        bottomNavigationView.setSelectedItemId(R.id.btn_home);
 
-            switch (item.getItemId()){
-                case R.id.button_lessons:
-                    replaceFragment(new Lessons_wFragment(), true);
-                    break;
-                case R.id.button_practice:
-                    replaceFragment(new Practice_wFragment(), true);
-                    break;
-                case R.id.button_profile:
-                    replaceFragment(new Profile_wFragment(), true);
-                    break;
-                case R.id.button_progress:
-                    replaceFragment(new Progress_wFragment(), true);
-                    break;
-                default:
-                    replaceFragment(new Lessons_wFragment(), true);
-                    break;
 
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (integerDeque.contains(id)){
+                    if(id == R.id.btn_home){
+                        if(integerDeque.size() != 1){
+                            if(flag){
+                                integerDeque.addFirst(R.id.btn_home);
+                                flag = false;
+
+                            }
+
+                        }
+
+                    }
+                    integerDeque.remove(id);
+                }
+                integerDeque.push(id);
+
+                loadFragment(getFragment(item.getItemId()));
+                return true;
             }
-
-
-            return true;
         });
 
-
     }
 
 
+
+    private Fragment getFragment(int itemId){
+
+        switch (itemId){
+
+            case R.id.btn_home:
+                bottomNavigationView.getMenu().getItem(0).setChecked(true);
+                return new HomeFragment();
+
+
+            case R.id.btn_Dashboard:
+                bottomNavigationView.getMenu().getItem(1).setChecked(true);
+                return new DashboardFragment();
+
+
+            case R.id.btn_Notification:
+                bottomNavigationView.getMenu().getItem(2).setChecked(true);
+                return new NotificationFragment();
+
+            case R.id.btn_Item:
+                bottomNavigationView.getMenu().getItem(3).setChecked(true);
+                return new ItemFragment();
+
+        }
+        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+        return new HomeFragment();
+    }
+
+
+    private void loadFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment,fragment,fragment.getClass().getSimpleName())
+                .commit();
+    }
 
     @Override
-    public void onBackPressed() {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        int count = fragmentManager.getBackStackEntryCount();
+    public void onBackPressed(){
+        integerDeque.pop();
 
-        if (count == 0) {
-            super.onBackPressed(); // Стандартное поведение, если стек возврата пуст
-        } else {
-            fragmentManager.popBackStack(); // Возврат к предыдущему фрагменту из стека возврата
+        if(!integerDeque.isEmpty()){
+            loadFragment(getFragment(integerDeque.peek()));
+        }else{
+            finish();
         }
     }
 
-    private void replaceFragment(Fragment fragment, boolean addToBackStack) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout_w, fragment);
 
-        if (addToBackStack) {
-            fragmentTransaction.addToBackStack(null); // Добавление в стек возврата, если нужно
-        }
 
-        fragmentTransaction.commit();
-    }
 
 
 }
-
-
-
