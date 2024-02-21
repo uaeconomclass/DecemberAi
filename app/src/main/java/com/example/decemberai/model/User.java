10bff800f8c71afe999da6bd6da60e35125c84f9
wowyc <149575541+wowyc@users.noreply.github.com>
2024-02-21 18:06:50 +0300

    disain finish

--- app/src/main/java/com/example/decemberai/model/User.java
+++ app/src/main/java/com/example/decemberai/model/User.java
@@ -6,10 +6,40 @@ import java.util.Set;
 public class User {
     public static Set<Integer> lessons_item_id = new HashSet<>(); // Set отличается от List тем что Set не дублирует поля, если встречается дубль он его не записывает
     public static Set<Integer> practice_item_id = new HashSet<>();
-
-    int userId = 1;
-
+    String userEmail = "wowyc26@gmail.com";
+    String userPassword = "1111";
+    int userId = 1;// Наверно не нужно
     String userLevel = "Новичок";
+    int userSkill = 15;
+
+
+
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getUserSkill() {
+        return userSkill;
+    }
+
+    public void setUserSkill(int userSkill) {
+        this.userSkill = userSkill;
+    }
+
+
 
     public int getUserId() {
         return userId;
